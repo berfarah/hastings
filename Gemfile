c38ba27b3d599d29e9b4ff73d24df4b90b493cf1
Bernardo Farah <ber@bernardo.me>
2015-07-23 12:14:32 -0700

    Adding testing dependencies

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in hastings.gemspec
 gemspec
+
+group :test do
+  gem "simplecov"
+  gem "fakefs", require: false
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
+require "fakefs/spec_helpers"
+RSpec.configure do |c|
+  c.include FakeFS::SpecHelpers, fakefs: true
+end
+
+require "simplecov"
+SimpleCov.start { add_filter "_spec" }
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "hastings"
