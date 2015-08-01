2badd9c2fdaa9842951d63276b5e6d7e17602175
Bernardo Farah <bfarah@walmart.com>
2015-07-31 17:44:13 -0700

    Implemented Mount extension

---
+++ lib/hastings/ext/mount/proxy_dir.rb
@@ -0,0 +1,14 @@
+module Hastings
+  module Mount
+    # Proxy for mounted directories
+    class ProxyDir < Dir
+      attr_reader :share, :remote_path
+
+      def initialize(share)
+        @remote_path = share.path
+        @share       = share
+        super(share.local_full_path)
+      end
+    end
+  end
+end
